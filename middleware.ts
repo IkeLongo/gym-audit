d0af72d32bab3202f03098d8fbe03d55f10220a4
Isaac Longoria <isaac.longoria9136@gmail.com>
2026-02-10 15:41:58 -0600

    Adjusted matcher config in middleware

--- middleware.ts
+++ middleware.ts
@@ -32,6 +32,14 @@ export function middleware(req: NextRequest) {
 
 export const config = {
   matcher: [
-    "/((?!_next|favicon.ico|robots.txt|sitemap.xml).*)",
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     * - Files with extensions (images, etc)
+     */
+    "/((?!api|_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp|avif|ico|bmp|pdf)).*)",
   ],
 };
